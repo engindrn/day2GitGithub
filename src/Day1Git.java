912cb26815e3f53440d4612418ade4fdf6d9c0d6
Engin Durna <eng.durna@gmail.com>
2022-08-13 22:12:23 +0200

    yeniclass

--- src/Day1Git.java
+++ src/Day1Git.java
@@ -2,6 +2,6 @@ public class Day1Git {
     public static void main(String[] args) {
         System.out.println("hallo git 1");
         System.out.println("hallo git 2");
-        System.out.println("hallo git 3");
+        System.out.println("hallo git 3.");
     }
 }
